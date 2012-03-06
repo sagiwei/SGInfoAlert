ba1408a682d106e6bc3ac8fff43ebfa538255acc
sagiwei <sagiwei@gmail.com>
2012-03-06 10:09:12 +0800

    Update SGInfoAlert.h

--- SGInfoAlert.h
+++ SGInfoAlert.h
@@ -1,8 +1,7 @@
 //
-//  SGInfoAlert.h
-//  Daily Carb
+//  SGInfoAlert.m
 //
-//  Created by Maxwell YQ003 on 3/1/12.
+//  Created by Azure_Sagi on 3/1/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
